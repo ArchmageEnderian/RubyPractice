0c1035e3061d0dfadb430f1f75c5ec856c40e40f
ArchmageEnderian <iggor.malakhov@gmail.com>
2023-07-31 21:15:14 +0300

    Add Task2 if not exists :D

--- Homework/Lesson5/scripts/files_methods.rb
+++ Homework/Lesson5/scripts/files_methods.rb
@@ -1,20 +1,24 @@
 # frozen_string_literal: true
 
+# Задание: Написать набор методов для работы с файлом
+
 class FileMaster
   # Global variables
 
   def index
-    puts @file_data
+    @file_data
   end
 
   def find(id)
-    puts @file_data[id - 1]
+    @file_data[id - 1]
   end
 
   def where(pattern)
+    @mass_of_findes = []
     @file_data.each do |line|
-      puts line if line.include?(pattern)
+      @mass_of_findes.append(line) if line.include?(pattern)
     end
+    @mass_of_findes
   end
 
   def update(id, text)
@@ -31,27 +35,29 @@ class FileMaster
     rewrite_file
   end
 
-  def rewrite_file
-    File.write(@file_name, @file_data.join("\n"))
+  def rewrite_file(file_path = @file_path, file_data = @file_data)
+    File.write(file_path, file_data.join("\n"))
   end
 
-  def initialize(file_name)
-    file = File.open(file_name)
-    @file_name = file_name
+  def initialize(file_path)
+    file = File.open(file_path)
+    @file_path = file_path
     @file_data = file.read.split("\n")
     file.close
+
+    @mass_of_findes = []
   end
 
 end
 
-file_master = FileMaster.new('Homework/Lesson4/files/file_to_read.txt')
+file_master = FileMaster.new('Homework/Lesson5/files/file_to_read.txt')
 
-file_master.index
-file_master.find(1)
-file_master.where('Ivan')
-file_master.update(2, "Phoenix Wright 26")
-file_master.delete(3)
-file_master.create("Phoenix Wright 33")
+# file_master.index
+# file_master.find(1)
+# file_master.where('58')
+# file_master.update(2, "Phoenix Wright 26")
+# file_master.delete(3)
+# file_master.create("Phoenix Wright 33")
 
 
 
